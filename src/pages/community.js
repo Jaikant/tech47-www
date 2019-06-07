1e31bd0fe5d42079c2b44c749ffc9732cd491bc9
tech47 <aayushineeshu@gmail.com>
2019-06-07 11:51:39 +0530

    Communitylanding

---
+++ src/pages/community.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import CommunityLanding from '../components/CommunityLanding';
+
+const community = () => (
+    <div>
+            <NavBar />
+        <CommunityLanding />
+    </div>
+)
+
+export default community
