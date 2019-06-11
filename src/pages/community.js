2da1b82d07c7433ae2fbfdd8d497dd8535605211
tech47 <aayushineeshu@gmail.com>
2019-06-11 18:54:06 +0530

    Community page with layout

--- src/pages/community.js
+++ src/pages/community.js
@@ -5,7 +5,6 @@ import CommunityLanding from '../components/CommunityLanding';
 const community = () => (
     <Layout>
       <CommunityLanding />
-      <div style={{minHeight: '100vh', background: 'green'}}/>
     </Layout>
 )
 
