6894785b614aef839cb863074f7f1a1ee5ce6710
Amanpreet-03 <amanpreet6309108@gmail.com>
2019-06-11 15:46:45 +0530

    ServicesCareer Component

--- src/components/ServicesCareer.js
+++ src/components/ServicesCareer.js
@@ -5,6 +5,13 @@ import Career from '../assets/images/Career.png'
 import TextImage from '../assets/images/TextImage.jpg'
 import Cloud from '../assets/images/Cloud.jpg'
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+`
+
 const Text = styled.div`
   font-size: 48px;
   width: 376px;
@@ -18,13 +25,13 @@ const ImageUnderText = styled(Text)`
   -webkit-text-fill-color: transparent;
 `
 
-const InnerWrapper = styled.div`
+const Upper = styled.div`
   background-image: url(${Career});
   background-repeat: no-repeat;
   background-size: 100% 100%;
 `
 
-const SecondWrapper = styled.div`
+const Lower = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   align-items: center;
@@ -32,17 +39,19 @@ const SecondWrapper = styled.div`
 
 const ServicesCareer = () => (
   <div>
-    <InnerWrapper>
-      <ImageUnderText>Hire React Developers & teams</ImageUnderText>
-      <ArrowButton text="Get in touch" white />
-    </InnerWrapper>
-    <SecondWrapper>
-      <div>
-        <Text>Work for tech47</Text>
-        <ArrowButton text="Open positions" white />
-      </div>
-      <img src={TextImage} width="622px" height="284px" alt="TextImage" />
-    </SecondWrapper>
+    <Wrapper>
+      <Upper>
+        <ImageUnderText>Hire React Developers & teams</ImageUnderText>
+        <ArrowButton text="Get in touch" white />
+      </Upper>
+      <Lower>
+        <div>
+          <Text>Work for tech47</Text>
+          <ArrowButton text="Open positions" white />
+        </div>
+        <img src={TextImage} width="622px" height="284px" alt="TextImage" />
+      </Lower>
+    </Wrapper>
     <BottomLine />
   </div>
 )
