188cfc701b8a807f30f0a00ab3fd649547a62f3f
Amanpreet-03 <amanpreet6309108@gmail.com>
2019-06-11 14:51:29 +0530

    WhoWeAre Component

--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'react-emotion';
 import { BottomLine } from './Common';
+import Cloud from '../assets/images/cloud.jpg';
 
 const Wrapper = styled.div`
     display: grid;
@@ -12,19 +13,29 @@ const Wrapper = styled.div`
     padding: 0px 80px 0px 40px;
 `
 
-const Text = styled.div`
+const LeftText = styled.div`
     font-size: 48px;
+    color: white;
+    background: url(${Cloud});
+    background-repeat: no repeat;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 `
 
-const Text2 = styled.div`
+const RightText = styled.div`
     font-size: 36px;
+    color: white;
+    background: url(${Cloud});
+    background-repeat: no repeat;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 `
 
 const WhoWeAre = () => (
     <div>
     <Wrapper>
-    <Text>Who are we</Text>
-    <Text2> You need an agency which understands your business and works as your partner?<br /><br /> We are a group of developers who have built great products which scale. Our teams come with a mix of experience to give the perfect balance.<br /><br /> We love the modern web. We are open source contributors. We also run a community of developers to share and spread knowledge.</Text2>
+    <LeftText>Who are we</LeftText>
+    <RightText> You need an agency which understands your business and works as your partner?<br /><br /> We are a group of developers who have built great products which scale. Our teams come with a mix of experience to give the perfect balance.<br /><br /> We love the modern web. We are open source contributors. We also run a community of developers to share and spread knowledge.</RightText>
     </Wrapper>
     <BottomLine />
     </div>
