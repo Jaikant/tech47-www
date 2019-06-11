cff03f6125cdabce220a2b77bca44b74ba2e3685
tech47 <aayushineeshu@gmail.com>
2019-06-11 18:18:55 +0530

    Form component

--- src/components/Common.js
+++ src/components/Common.js
@@ -4,9 +4,12 @@ import Arrow from '../assets/icons/Arrow.svg';
 import BlackArrow from '../assets/icons/BlackArrow.svg';
 import Partition from '../assets/icons/Partition.svg';
 
-const Wrapper = styled.div`
+const Wrapper = styled.button`
     display: flex;
     align-items: baseline;
+    background-color: transparent;
+    border: none;
+    outline: none;
 `
 const Text = styled.div`
     font-size: 18px;
@@ -17,10 +20,10 @@ const Text = styled.div`
 const PartitionWrapper = styled.div`
     display: flex;
     justify-content: center;
-`
+`;
 
 export const ArrowButton = (props) => (
-    <Wrapper>
+    <Wrapper onClick={props.onClick}>
        <Text {...props}>{props.text}</Text>
        {props.white ? <img src={Arrow} width="24" height="12" alt="Arrow" /> : <img src={BlackArrow} width="24" alt="BlackArrow" />}
     </Wrapper>
