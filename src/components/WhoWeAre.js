c1a6c3753a771e799c81ee74b998a2110d572aed
Amanpreet-03 <amanpreet6309108@gmail.com>
2019-06-14 11:07:37 +0530

    changes in WhoWeAre component

--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import styled from 'react-emotion';
 import { BottomLine } from './Common';
-import Cloud from '../assets/images/cloud.jpg';
+import Cloud from '../assets/images/CloudImage.jpg';
 
 const Wrapper = styled.div`
     display: grid;
     grid-template-columns: 0.5fr 4fr;
     grid-gap: 50px;
     align-items: center;
-    align-content: center;
     height: 100vh;
-    padding: 0px 80px 0px 40px;
 `
 
 const LeftText = styled.div`
@@ -33,11 +31,11 @@ const RightText = styled.div`
 
 const WhoWeAre = () => (
     <div>
-    <Wrapper>
-    <LeftText>Who are we</LeftText>
-    <RightText> You need an agency which understands your business and works as your partner?<br /><br /> We are a group of developers who have built great products which scale. Our teams come with a mix of experience to give the perfect balance.<br /><br /> We love the modern web. We are open source contributors. We also run a community of developers to share and spread knowledge.</RightText>
-    </Wrapper>
-    <BottomLine />
+        <Wrapper>
+            <LeftText>Who are we</LeftText>
+            <RightText> You need an agency which understands your business and works as your partner?<br /><br /> We are a group of developers who have built great products which scale. Our teams come with a mix of experience to give the perfect balance.<br /><br /> We love the modern web. We are open source contributors. We also run a community of developers to share and spread knowledge.</RightText>
+        </Wrapper>
+        <BottomLine />
     </div>
 )
 
