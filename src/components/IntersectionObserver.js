02b3173a919a35c29129ac5d88619a336241b446
Jai <jaikant@gmail.com>
2019-06-22 18:21:33 +0530

    intersection observer based animation for one comp.

---
+++ src/components/IntersectionObserver.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+
+class IntersectionObserver extends Component {
+  element = React.createRef()
+
+  state = {
+    boundingClientRect: {},
+    visiblePercentage: 0,
+    isIntersecting: false,
+    visible: false,
+    entering: false,
+    exiting: false,
+  }
+
+  componentDidMount() {
+    if (this.props.element !== 'placeholder') {
+      this.setupObserver()
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.observer && typeof this.observer.disconnect === 'function') {
+      this.observer.disconnect()
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.element !== prevProps.element) {
+      this.setupObserver()
+    }
+  }
+
+  setupObserver = () => {
+    // console.log(JSON.stringify(this.generateObserverOptions()));
+    const $el = this.props.element || this.element.current
+    this.observer = new window.IntersectionObserver(entries => {
+      const element = entries[0]
+      this.handleObservation(element)
+
+      this.hasStartedObservation = true
+    }, this.generateObserverOptions())
+
+    this.observer.observe($el)
+  }
+
+  handleObservation = element => {
+    const boundingClientRect = element.boundingClientRect
+    const eventType = boundingClientRect.top > 0 ? 'entering' : 'exiting'
+    const visiblePercentage = Math.floor(element.intersectionRatio * 100)
+
+    this.setState({
+      boundingClientRect,
+      isIntersecting: element.isIntersecting,
+      entering: eventType === 'entering',
+      exiting: eventType === 'exiting',
+      visible: visiblePercentage > 0,
+      visiblePercentage,
+      intersectionRatio: element.intersectionRatio,
+    })
+  }
+
+  generateObserverOptions = () => {
+    const threshold = []
+
+    for (let i = 0; i <= 1.0; i += 0.01) {
+      threshold.push(i)
+    }
+
+    // Setting defaults for options, but overriding with props if provided
+    return {
+      root: null,
+      rootMargin: '0px',
+      threshold,
+      ...this.props.options,
+    }
+  }
+
+  render() {
+    return (
+      <div data-component="intersection-observer" ref={this.element}>
+        {this.props.children(this.state)}
+      </div>
+    )
+  }
+}
+
+export default IntersectionObserver
--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -1,83 +1,160 @@
-import React from 'react';
-import styled from 'react-emotion';
-import { BottomLine } from './Common';
-import Cloud from '../assets/images/CloudImage.jpg';
-import media from '../utils/media';
+import React, { useEffect, useRef } from 'react'
+import styled from 'react-emotion'
+import Cloud from '../assets/images/CloudImage.jpg'
+import media from '../utils/media'
+import IntersectionObserver from '../components/IntersectionObserver'
+import { useSpring, animated } from 'react-spring'
+import throttle from 'lodash/throttle';
+import getWindowDimensions from '../utils/getWindowDimensions';
 
 const Wrapper = styled.div`
-    display: grid;
-    grid-template-rows: 1fr;
-    grid-template-columns: 1fr;
-    ${media.tablet`
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr;
+  ${media.tablet`
         grid-template-columns: 1fr;
         grid-template-rows: 1fr;
     `};
-    grid-gap: 48px;
-    align-items: center;
-    padding-top: 32px;
-    ${media.tablet`
-        padding-top: 64px;
-    `};
-    ${media.desktop`
-        padding-top: 128px;
-    `};  
-
-    min-height: calc(100vh - 32px);
-    ${media.tablet`
-      min-height: calc(100vh - 64px);
-    `};
-    ${media.desktop`
-      min-height: calc(100vh - 128px);
-    `};  
+  grid-gap: 48px;
+  align-items: center;
+  min-height: 100vh;
 `
 
 const ImageUnderText = styled.div`
-    color: white;
-    background: url(${Cloud});
-    background-repeat: no repeat;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+  color: white;
+  background: url(${Cloud});
+  background-repeat: no repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+const LeftTextWrapper = styled.div`
+  position: sticky;
+  top: 0px;
+  padding-bottom: 128px;
 `
 
 const LeftText = styled.div`
-    position: sticky;
-    top: 128px;
-    padding-bottom: 64px;
-    align-self: flex-start;
-    font-size: 48px;
+  align-self: flex-start;
+  font-size: 48px;
 `
 
 const RightText = styled(ImageUnderText)`
-    align-self: flex-start;
-    font-size: 32px;
+  align-self: flex-start;
+  font-size: 32px;
+  opacity: ${p => p.opacity};
 `
 
-const WhoWeAre = () => (
-    <div>
-        <Wrapper>
-            <LeftText data-scroll-fade={true}>Who are we</LeftText>
-            <div>
-                <RightText data-scroll-fade-early={true}> 
-                    You need an agency which understands your business and works as your partner? 
-                </RightText>
-                <br />
-                <br /> 
-                <RightText data-scroll-fade-early={true}> 
-                    We are a group of developers who have built great products which scale. 
-                    Our teams come with a mix of experience to give the perfect balance.
-                </RightText>
-                <br />
-                <br /> 
-                <RightText data-scroll-fade-early={true}> 
-                    We love the modern web. We are open source contributors. 
-                    We also run a community of developers to share and spread knowledge.
-                </RightText>
-                <br />
-                <br /> 
-            </div>
-        </Wrapper>
-        <BottomLine />
-    </div>
-)
+const calcTransform = (offset) => `translateY(${offset * 180}px)`;
 
-export default WhoWeAre;
+const HeaderWhoAreWe = () => {
+  const config = { mass: 2, tension: 300, friction: 120 }
+
+  const [args, set] = useSpring(() => ({
+    offset: 1,
+    config,
+  }))
+
+  const animatedStyles = {
+    transform: args.offset.interpolate(calcTransform),
+    position: 'relative',
+    zIndex: 2,
+    pointerEvents: 'none',
+  }
+
+  const heading = useRef();
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      if (heading.current) {
+        const { height } = getWindowDimensions()
+        const offset = heading.current.getBoundingClientRect().top
+        set({ offset: 1 - offset / height })
+      }
+    })
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <LeftTextWrapper data-scroll-fade={true} innerRef={heading}>
+          <animated.div style={animatedStyles}>
+
+      <LeftText>Who are we</LeftText>
+      </animated.div>
+
+    </LeftTextWrapper>
+  )
+}
+
+const WhoWeAre = () => {
+  return (
+    <div>
+      <Wrapper>
+        <HeaderWhoAreWe />
+        <div>
+          <IntersectionObserver options={{rootMargin : '-350px 0px 0px 0px'}}>
+            {props => {
+                const {entering, exiting, isIntersecting, visible, intersectionRatio, visiblePercentage, boundingClientRect} = props;
+                // console.log(`isIntersecting : ${isIntersecting} exiting : ${exiting} intersectionRatio : ${intersectionRatio}, visiblePercentage: ${visiblePercentage}, boundingClientRect: ${boundingClientRect}`, JSON.stringify(boundingClientRect));
+              return (
+                  <RightText opacity={props.intersectionRatio} exiting={props.exiting}>
+                    You need an agency which understands your business and works
+                    as your partner?
+                  </RightText>
+              )
+            }}
+          </IntersectionObserver>
+          <br />
+          <br />
+          <IntersectionObserver options={{rootMargin : '-290px 0px 0px 0px'}}>
+            {props => {
+              return (
+                  <RightText opacity={props.intersectionRatio} exiting={props.exiting}>
+                    We are a group of developers who have built great products
+                    which scale. 
+                  </RightText>
+              )
+            }}
+          </IntersectionObserver>          
+          <IntersectionObserver options={{rootMargin : '-290px 0px 0px 0px'}}>
+            {props => {
+              return (
+                  <RightText opacity={props.intersectionRatio} exiting={props.exiting}>
+                    Our teams come with a mix of experience to give
+                    the perfect balance.
+                  </RightText>
+              )
+            }}
+          </IntersectionObserver>
+          <br />
+          <br />
+          <IntersectionObserver options={{rootMargin : '-290px 0px 0px 0px'}}>
+            {props => {
+              return (
+                  <RightText opacity={props.intersectionRatio} exiting={props.exiting}>
+                    We love the modern web. We are open source contributors. 
+                  </RightText>
+              )
+            }}
+          </IntersectionObserver>
+          <IntersectionObserver options={{rootMargin : '-290px 0px 0px 0px'}}>
+            {props => {
+              return (
+                  <RightText opacity={props.intersectionRatio} exiting={props.exiting}>
+                    We also run a community of developers to share and spread
+                    knowledge.
+                  </RightText>
+              )
+            }}
+          </IntersectionObserver>
+          <br />
+          <br />
+        </div>
+      </Wrapper>
+    </div>
+  )
+}
+
+export default WhoWeAre
