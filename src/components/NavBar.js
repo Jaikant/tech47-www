060c821267d1a77849732e1b787c69f1bea46092
Jai <jaikant@gmail.com>
2019-06-24 10:19:49 +0530

    navbar in progress

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,6 @@ import colors from '../utils/colors';
 import styled from 'react-emotion'
 import media from '../utils/media';
 
-
-const Title = styled.text`
-&:hover {
-   color: white;
- }
-`
-
 const menuConfig = [
    // { title: 'BLOG', url: '/blogs', submenu: false },
    // { title: 'CONTACT', url: '/contact', submenu: false },
@@ -25,11 +18,96 @@ const menuConfig = [
  const NavWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
-`
+  height: ${p => p.open? '100%' : '50px'};
+  transition: ${p => p.open ? 'all 0.45s ease-out, background 0.8s ease-out' :
+    'all 0.4s ease-out, background 1s ease-out'};
+`;
+
+const MobileMenu = styled.div`
+  display: block;
+  ${media.tablet`
+    display: none;
+  `};
+`;
+
+const MobileUl = styled.ul`
+  position: relative;
+  display: block;
+  padding: 0px 40px 0;
+  list-style: none;
+`;
+
+const MobileLi = styled.li`
+  border-bottom: 1px solid #333;
+  margin-top: 5px;
+  & > a {
+    display: block;
+    position: relative;
+    color: #fff;
+    text-decoration: none;
+    font-size: 18px;
+    line-height: 2.8;
+    width: 100%;
+    -webkit-tap-highlight-color: transparent;
+  }
+  opacity: ${p => p.open ? 1 : 0};
+  transition: opacity 0.6s cubic-bezier(0.4, 0.01, 0.165, 0.99), -webkit-transform 0.5s cubic-bezier(0.4, 0.01, 0.165, 0.99);
+  transition: transform 0.5s cubic-bezier(0.4, 0.01, 0.165, 0.99), opacity 0.6s cubic-bezier(0.4, 0.01, 0.165, 0.99);
+  transition: transform 0.5s cubic-bezier(0.4, 0.01, 0.165, 0.99), opacity 0.6s cubic-bezier(0.4, 0.01, 0.165, 0.99), -webkit-transform 0.5s cubic-bezier(0.4, 0.01, 0.165, 0.99);
+  &:nth-child(1) {
+    transition-delay: ${p => p.open ? '0.05s' : '0.35s'};
+  }
+  &:nth-child(2) {
+    transition-delay: ${p => p.open ? '0.1s' : '0.3s'};
+  }
+  &:nth-child(3) {
+    transition-delay: ${p => p.open ? '0.15s' : '0.25s'};
+  }
+  &:nth-child(4) {
+    transition-delay: ${p => p.open ? '0.20s' : '0.20s'};
+  }
+  &:nth-child(5) {
+    transition-delay: ${p => p.open ? '0.25s' : '0.15s'};
+  }
+  &:nth-child(6) {
+    transition-delay: ${p => p.open ? '0.30s' : '0.10s'};
+  }
+  &:nth-child(7) {
+    transition-delay: ${p => p.open ? '0.35s' : '0.05s'};
+  }
+`;
+
+const MobileMenuItems = (props) => (
+  <MobileMenu>
+    <MobileUl>
+      <MobileLi {...props}>
+        <a href="#">Mac</a>
+      </MobileLi>
+      <MobileLi>
+        <a href="#">iPad</a>
+      </MobileLi>
+      <MobileLi>
+        <a href="#">iPhone</a>
+      </MobileLi>
+      <MobileLi>
+        <a href="#">Watch</a>
+      </MobileLi>
+      <MobileLi>
+        <a href="#">TV</a>
+      </MobileLi>
+      <MobileLi>
+        <a href="#">Music</a>
+      </MobileLi>
+      <MobileLi>
+        <a href="#">Support</a>
+      </MobileLi>
+    </MobileUl>
+  </MobileMenu>
+)
+
 
 const MenuItem = styled.ul`
-  display: flex;  
+  display: none;  
   ${media.tablet`
     display: flex;
   `};
@@ -114,8 +192,7 @@ const animateIn = [
    { width: '50%', transform: 'initial', opacity: '0.5' },
  ]
  
-const MenuBar = ({ menuRef }) => {
-  const [open, setOpen] = useState(false);
+const MenuBar = ({ menuRef, open }) => {
 
   useEffect(() => {
      if (open) {
@@ -132,13 +209,9 @@ const MenuBar = ({ menuRef }) => {
        })
      }
   });
-
-  const showMenu = () => {
-    setOpen(current => !current)
-  }
   
   return (
-   <div onClick={showMenu}>
+   <div>
       <MorphReplaceResize width="16" height="16" rotation={"none"}>
          {open ? <MenuVertical key="open"/> : <Menu key="closed"/>}
       </MorphReplaceResize>
@@ -152,14 +225,23 @@ const LogoImg = styled.img`
 
 const NavBar = props => {
    const menuRef = useRef();
+   const [open, setOpen] = useState(false);
 
+   const showMenu = () => {
+     console.log('...')
+     setOpen(current => !current)
+   }
+ 
   return (
-    <NavWrapper {...props}>
+    <NavWrapper {...props} open={open}>
       <Link to="/">
         <LogoImg src={Logo} width="110" height="30" alt="Logo" /> 
       </Link>
       <MenuItems ref={menuRef}/>
-      <MenuBar menuRef={menuRef} />
+      <MobileMenuItems open={open} />
+      <div onClick={showMenu}>
+        <MenuBar menuRef={menuRef} open={open} />
+      </div>
     </NavWrapper>
   )
 }
